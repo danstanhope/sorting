b9d5899fb82a9f20cf854fa9b882b66a3310f09c
Dan Stanhope <danstanhope@gmail.com>
2013-05-03 13:12:02 -0400

    sorting

--- sorting.js
+++ sorting.js
@@ -3,7 +3,6 @@
 
 	sorting = {
 		insertionSort : function(arr){
-
 			var j = 0, _tmp = [];
 
 			for(var p = 1; p < arr.length; p++){
@@ -18,7 +17,19 @@
 			return arr;
 		},
 		shellSort : function(arr){
+			var j = 0, _tmp = [];
 
+			for(var gap = Math.ceil(arr.length / 2); gap > 0; Math.ceil(gap /= 2)){
+				for(var i = gap; i < arr.length; i++){
+					_tmp = arr[i];
+
+					for(j = i ; j >= gap && _tmp < arr[j - gap]; j -= gap){
+						arr[j] = arr[j - gap];
+						console.log(j -= gap)
+					}
+					arr[j] = _tmp;
+				}
+			}
 			return arr;
 		}
 	};
